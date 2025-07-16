19ef75316d167c0d1fd5e39105874aa85e549304
VladikNV250 <vladiknemichev@gmail.com>
2025-07-17 00:59:21 +0300

    refactor(FE-50): style-refactored widgets layer

--- src/widgets/stop-info-panel/ui/StopInfoPanel/StopInfoPanel.tsx
+++ src/widgets/stop-info-panel/ui/StopInfoPanel/StopInfoPanel.tsx
@@ -11,8 +11,6 @@ import { formatDate, useAppSelector } from 'shared/lib';
 import { SimpleLoader } from 'shared/ui';
 import { extractCityAndRegion } from 'widgets/stop-info-panel/lib';
 
-import styles from './style.module.scss';
-
 export const StopInfoPanel: FC = () => {
 	const currentStop = useAppSelector(selectCurrentStop);
 	const { weather, loading: weatherLoading } = useWeather(currentStop);
@@ -25,59 +23,70 @@ export const StopInfoPanel: FC = () => {
 	);
 
 	return (
-		<div className={clsx(styles.stopInfoPanel, hasStop && styles.active)}>
+		<div
+			className={clsx(
+				'animate-slide-fade-out absolute top-0 left-0 z-10 w-full bg-linear-to-l from-sky-300 to-blue-900 opacity-0',
+				hasStop && 'animate-slide-fade-in',
+			)}
+		>
 			{hasStop && (
 				<>
-					<header className={styles.header}>
-						<div className={styles.titleContainer}>
-							<h3 className={styles.title}>{city}</h3>
+					<header className="mb-3 flex items-center justify-between">
+						<div className="flex flex-col text-neutral-50">
+							<h3 className="text-2xl font-medium">{city}</h3>
 							<h4>{region}</h4>
 						</div>
 						<TripContinueButton />
 					</header>
-					<div className={styles.widgetContainer}>
-						<div className={styles.infoWidget}>
-							<header className={styles.infoHeader}>
-								<h4 className={styles.infoTitle}>Weather</h4>
-								<h4 className={styles.infoSubtitle}>On {displayDate}</h4>
+					<div className="grid grid-flow-col grid-cols-3 gap-4">
+						<div className="max-h-27.5 min-h-15 w-full overflow-y-hidden rounded-xl bg-white/40 p-3">
+							<header className="flex items-center justify-between">
+								<h4 className="text-xl font-medium text-neutral-50">Weather</h4>
+								<h4 className="text-base text-neutral-50 italic">
+									On {displayDate}
+								</h4>
 							</header>
 							{weatherLoading ? (
 								<SimpleLoader loading={true} />
 							) : weather ? (
-								<div className={styles.weatherContainer}>
-									<div className={styles.temperatureContainer}>
-										<p className={styles.temperature}>
+								<div className="flex items-center justify-between">
+									<div>
+										<p className="text-lg font-medium text-neutral-50">
 											{weather.temperature}&#8451;
 										</p>
-										<p className={styles.condition}>{weather.condition}</p>
+										<p className="text-sm text-neutral-100">
+											{weather.condition}
+										</p>
 									</div>
 									<img
 										src={weather.icon}
 										alt={weather.condition}
-										className={styles.conditionIcon}
+										className="size-12"
 									/>
 								</div>
 							) : (
-								<p className={styles.condition}>
+								<p className="text-sm text-neutral-100">
 									Weather is temporarily unavailable
 								</p>
 							)}
 						</div>
-						<div className={styles.infoWidget}>
-							<h4 className={styles.infoTitle}>Holidays</h4>
+						<div className="max-h-27.5 min-h-15 w-full overflow-y-hidden rounded-xl bg-white/40 p-3">
+							<h4 className="text-xl font-medium text-neutral-50">Holidays</h4>
 							{holidayLoading ? (
 								<SimpleLoader />
 							) : holiday ? (
-								<p className={styles.infoSubtitle}>{holiday.name}</p>
+								<p className="text-base text-neutral-50 italic">
+									{holiday.name}
+								</p>
 							) : (
-								<p className={styles.infoSubtitle}>
+								<p className="text-base text-neutral-50 italic">
 									There are no holidays on this date
 								</p>
 							)}
 						</div>
-						<div className={styles.infoWidget}>
-							<h4 className={styles.infoTitle}>Notes</h4>
-							<p className={styles.notes}>
+						<div className="max-h-27.5 min-h-15 w-full overflow-y-hidden rounded-xl bg-white/40 p-3">
+							<h4 className="text-xl font-medium text-neutral-50">Notes</h4>
+							<p className="no-scrollbar h-full overflow-y-hidden text-sm text-neutral-50">
 								{currentStop?.notes || 'No notes about this place.'}
 							</p>
 						</div>
